e43f3b3768aeb928a9f27faebdda2426e23ce7f3
Eugene Yokota <eed3si9n@gmail.com>
2022-10-01 17:37:46 -0400

    Scala 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val sbtV = "1.6.0"
-  val scala3 = "3.1.1"
+  val scala3 = "3.2.0"
 
   val verify = "com.eed3si9n.verify" %% "verify" % "1.0.0"
   val scalaCompiler = "org.scala-lang" %% "scala3-compiler" % scala3
