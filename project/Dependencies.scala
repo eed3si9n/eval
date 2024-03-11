ec3429a2235c0d175d1aa0b2dd2d2b2bd7240479
Eugene Yokota <eed3si9n@gmail.com>
2024-03-11 12:40:39 -0400

    Scala 3.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  val sbtV = "1.9.7"
-  val scala3 = "3.3.1"
+  val sbtV = "1.9.9"
+  val scala3 = "3.4.0"
 
   val verify = "com.eed3si9n.verify" %% "verify" % "1.0.0"
   val scalaCompiler = "org.scala-lang" %% "scala3-compiler" % scala3
