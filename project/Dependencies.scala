0c0d43a1fecba90e68e8fe3726f19d51b5a47b06
Eugene Yokota <eed3si9n@gmail.com>
2025-04-27 22:35:35 -0400

    Cross build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,6 @@ import sbt._
 
 object Dependencies {
   val sbtV = "1.9.9"
-  val scala3 = "3.6.4"
-
   val verify = "com.eed3si9n.verify" %% "verify" % "1.0.0"
-  val scalaCompiler = "org.scala-lang" %% "scala3-compiler" % scala3
   val ioProj = "org.scala-sbt" %% "io" % sbtV
 }
